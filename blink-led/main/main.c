8548c22a2161826319e475f255d9451356d04cc5
gokulmaxi <gokulakams@gmail.com>
2020-06-01 17:53:18 +0530

    blinkled added

---
+++ blink-led/main/main.c
@@ -0,0 +1,37 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "driver/gpio.h"
+ 
+#define BLINK_GPIO 13
+
+void app_main(void)
+{
+  nvs_flash_init();
+    xTaskCreate(&blinky, "blinky", 512,NULL,5,NULL );
+}
+void blinky(void *pvParameter)
+{
+ 
+    gpio_pad_select_gpio(BLINK_GPIO);
+    /* Set the GPIO as a push/pull output */
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    while(1) {
+        /* Blink off (output low) */
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        /* Blink on (output high) */
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(1000 / portTICK_RATE_MS);
+    }
+}
